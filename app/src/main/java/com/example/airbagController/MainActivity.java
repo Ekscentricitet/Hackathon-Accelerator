de765884f2966498f5494e42bb600bc6206880f8
WalkeR <stefcho989@abv.bg>
2019-04-19 13:22:34 +0300

    Added sound when phone falling. Some commented code left that might be needed.

--- app/src/main/java/com/example/airbagController/MainActivity.java
+++ app/src/main/java/com/example/airbagController/MainActivity.java
@@ -7,6 +7,10 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -21,6 +25,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager sensorManager;
 // Defines an element from the Sensor group.
     Sensor accelerometer;
+// Defines the tone.
+    private SoundPool soundPool;
+    private int beep;
 
 // Defines the variables we will use to store the data.
     private TextView readingX, readingY, readingZ, absoluteValueText, thresholdTextView;
@@ -43,6 +50,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 // Sets the delay to zero.
         sensorManager.registerListener(MainActivity.this, accelerometer, sensorManager.SENSOR_DELAY_FASTEST);
         Log.d(TAG, "onCreate: Registered accelerometer listener");
+//Checks the system's version and declares the corresponding soundPool.
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+//            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+//                    .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+//                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+//                    .build();
+//
+//            soundPool = new SoundPool.Builder()
+//                    .setMaxStreams(1)
+//                    .setAudioAttributes(audioAttributes)
+//                    .build();
+//        }
+//        else{
+            soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC,0);
+//        }
+// Declare the beep's location.
+        beep = soundPool.load(this, R.raw.beep, 1);
 
 // Define the strings that we will store the data in.
         readingX = findViewById(R.id.axisX);
@@ -88,16 +112,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         absoluteValue = Math.sqrt((Math.pow(X, 2) + Math.pow(Y, 2) + Math.pow(Z, 2)));
 
-        absoluteValueText.setText(String.format("Absolute Value: %.4f", absoluteValue));
+        absoluteValueText.setText(String.format("Absolute Value: %.3f", absoluteValue));
 
        if(absoluteValue <= threshold){
-           ((TextView)findViewById(R.id.errorMessage)).setText("Ниско ускорение.");
            Button mButton = findViewById(R.id.colorButton);
            mButton.setBackgroundColor(Color.RED);
+           soundPool.play(beep, 1, 1, 1, 0, 1);
        }
        else{
            Button mButton = findViewById(R.id.colorButton);
            mButton.setBackgroundColor(Color.GREEN);
        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        soundPool.release();
+        soundPool = null;
     }
 }
